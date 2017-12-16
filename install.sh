9bdd7840b91e360da774df965e6e5f65923257e0
Hanjun Kim <hallazzang@gmail.com>
2017-12-17 04:48:38 +0900

    install.sh: print information of current process

--- install.sh
+++ install.sh
@@ -14,8 +14,10 @@ ln -sf $DOTFILES/zsh/zprofile $HOME/.zprofile
 
 # Homebrew
 if ! [ -x "$(command -v brew)" ]; then
+    echo "Installing Homebrew"
     /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
+echo "Updating Homebrew"
 brew update
 brew upgrade
 
@@ -26,6 +28,7 @@ ln -sf $DOTFILES/vim/vimrc $HOME/.vimrc
 
 # Tmux
 if ! [ -x "$(command -v reattach-to-user-namespace)" ]; then
+    echo "Installing reattach-to-user-namespace"
     brew install reattach-to-user-namespace
 fi
 ln -sf $DOTFILES/tmux/tmux.conf $HOME/.tmux.conf
